b4c5d907666cf9579f72fbe4640898cbda636a5f
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-10-05 11:55:22 -0700

    Update scmversion.h

--- src/scmversion/scmversion.h
+++ src/scmversion/scmversion.h
@@ -4,4 +4,6 @@ extern const char* g_scm_hash_str;
 extern const char* g_scm_branch_str;
 extern const char* g_scm_tag_str;
 extern const char* g_scm_date_str;
-
+extern const char* g_scm_tag_hi_str;
+extern const char* g_scm_tag_med_str;
+extern const char* g_scm_tag_lo_str;
