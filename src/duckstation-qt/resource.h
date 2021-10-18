0fd329e34d3ff2a2390b10b4eebf6f240602b7d8
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-10-17 19:06:05 -0700

    const auto

--- src/duckstation-qt/resource.h
+++ src/duckstation-qt/resource.h
@@ -17,9 +17,9 @@
 
 // Add Version info
 //
-#define DUCK_VersionHi     g_scm_tag_hi_str
-#define DUCK_VersionMid    g_scm_tag_med_str
-#define DUCK_VersionLo     g_scm_tag_lo_str
+const auto DUCK_VersionHi = string(g_scm_tag_hi_str);
+const auto DUCK_VersionMid = string(g_scm_tag_med_str);
+const auto DUCK_VersionLo = string(g_scm_tag_lo_str);
 
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
